487816ad97299d45d0656d98006114c7455ca225
Edgar <edguitian@imagine800.es>
2024-01-12 13:19:51 +0100

    Initial Commit

---
+++ MultipleFunctional/MultipleFunctionalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleFunctionalApp.swift
+//  MultipleFunctional
+//
+//  Created by Edgar Guitian Rey on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MultipleFunctionalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
